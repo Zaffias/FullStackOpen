f0ace42d572f0821f738f6bbabf27b9abbe586ce
Zaffias <rdpp08@gmail.com>
2022-06-09 22:50:36 +0200

    finished

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -4,12 +4,28 @@ import { StatisticsLine } from './StatisticsLine'
 export const Statistics = ({good, bad, neutral}) => {
   return (
     <div>
-        <StatisticsLine value={good} text='good'/>
-        <StatisticsLine value={neutral} text='neutral'/>
-        <StatisticsLine value={bad} text='bad'/>
-        <StatisticsLine value={bad + neutral + good} text='All'/>
-        <StatisticsLine value={(bad * -1 + good )/ (bad + good + neutral)} text='Average'/>
-        <StatisticsLine value={good / (bad + good + neutral) * 100} text='Positive'/>
+    <table>
+      <tbody>
+      <tr>
+        <td> <StatisticsLine value={good} text='good'/></td>
+      </tr>
+      <tr>
+        <td> <StatisticsLine value={neutral} text='neutral'/></td>
+      </tr>
+      <tr>
+        <td> <StatisticsLine value={bad} text='bad'/></td>
+      </tr>
+      <tr>
+        <td> <StatisticsLine value={bad + neutral + good} text='All'/></td>
+      </tr>
+      <tr>
+        <td> <StatisticsLine value={((bad * - 1 + good ) / (bad + good + neutral)).toFixed(2)} text='Average'/></td>
+      </tr>
+      <tr>
+        <td> <StatisticsLine value={(good / (bad + good + neutral) * 100).toFixed(2)} text='Positive'/></td>
+      </tr>
+     </tbody>
+    </table>
     </div>
   )
 }
